034b312341dc71894eb293758d8c3d46c52596d5
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-23 22:57:08 +0300

    Add files via upload

---
+++ kursova/Cafedra.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicDepartment.DomainEntities
+{
+    public class Cafedra : BaseEntity
+    {
+        private string name;
+
+        [Phone]
+        [MaxLength(64)]
+        public string Phone { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                    NormalizedName = Name.ToUpperInvariant();
+                }
+            }
+        }
+
+        [Required]
+        [MaxLength(64)]
+        public string NormalizedName { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string Description { get; set; }
+
+        public virtual List<Teacher> Teachers { get; set; } = new List<Teacher>();
+    }
+}
