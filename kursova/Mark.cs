3f4f6594cd56a7795d337391828703a3214f2232
moplyss <96358213+moplyss@users.noreply.github.com>
2022-05-23 21:44:49 +0300

    Update Mark.cs

--- kursova/Mark.cs
+++ kursova/Mark.cs
@@ -4,17 +4,13 @@ namespace ElectronicDepartment.DomainEntities
 {
     public class Mark : BaseEntity
     {
-        public int CurrentGroupId { get; set; }
-        public virtual Lesson GroupCourse { get; set; } = default!;
-
-        public int StudentId { get; set; }
+     public int LessonId { get; set; }
+        public virtual Lesson Lesson { get; set; } = default!;
+        
+        public string StudentId { get; set; }
 
         public virtual Student Student { get; set; } = default!;
         
-        public int CourseTeacherId { get; set; }
-
-        public virtual CourseTeacher CourseTeacher { get; set; } = default!;
-
         [Range(0, 100)]
         public int Value { get; set; }
     }
