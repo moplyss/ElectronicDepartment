1ce2c7b2973b4fcc32b66b4f761ff3832f3fdd5e
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-26 22:46:57 +0300

    Starting table

---
+++ ElectronicDepartment.Interfaces/ICourseTeacherService.cs
@@ -0,0 +1,26 @@
+﻿using ElectronicDepartment.Web.Shared;
+using ElectronicDepartment.Web.Shared.Common;
+using ElectronicDepartment.Web.Shared.CourseTeacher;
+using ElectronicDepartment.Web.Shared.CourseTeacher.Responce;
+
+namespace ElectronicDepartment.Interfaces
+{
+    public interface ICourseTeacherService
+    {
+        public Task<int> Create(CreateCourseTeacherViewModel viewModel);
+
+        public Task<IEnumerable<int>> CreateRange(IEnumerable<CreateCourseTeacherViewModel> viewModel);
+
+        public Task Update(UpdateCourseTeacherViewModel viewModel);
+
+        public Task<GetCourseTeacherViewModel> Get(int id);
+
+        public Task RemoveTeacher(int id);
+
+        public Task RemoveTeacherRange(IEnumerable<int> ids);
+
+        public Task<IEnumerable<GetCourseTeacherSelectorViewModel>> GetSelector();
+
+        public Task<ApiResultViewModel<GetCourseTeacherViewModel>> GetApiCourseTeacherResponce(int pageIndex, int pageSize, IEnumerable<SortingRequest> sortingRequests, IEnumerable<FilterRequest> filterRequests);
+    }
+}
---
+++ ElectronicDepartment.Interfaces/IGroupService.cs
@@ -0,0 +1,20 @@
+﻿using ElectronicDepartment.Web.Shared;
+using ElectronicDepartment.Web.Shared.Common;
+using ElectronicDepartment.Web.Shared.Group;
+using ElectronicDepartment.Web.Shared.Group.Responce;
+
+namespace ElectronicDepartment.Interfaces
+{
+    public interface IGroupService
+    {
+        public Task<GetGroupViewModel> Get(int id);
+
+        public Task<int> Create(CreateGroupViewModel viewModel);
+
+        public Task Update(UpdateGroupViewModel viewModel);
+
+        public Task<IEnumerable<GetGroupSelectorViewModel>> GetSelector();
+
+        public Task<ApiResultViewModel<GetGroupViewModel>> GetApiResponce(int pageIndex, int pageSize, IEnumerable<SortingRequest> sortingRequests, IEnumerable<FilterRequest> filterRequests);
+    }
+}
