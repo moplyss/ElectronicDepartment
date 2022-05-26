ed7a3fe6942181d6babf9375ee4d09df5b90cf58
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-26 22:43:50 +0300

    Starting table

---
+++ ElectronicDepartment.Interfaces/ICafedraService.cs
@@ -0,0 +1,23 @@
+﻿using ElectronicDepartment.Web.Shared;
+using ElectronicDepartment.Web.Shared.Cafedra;
+using ElectronicDepartment.Web.Shared.Cafedra.Responce;
+using ElectronicDepartment.Web.Shared.Common;
+
+namespace ElectronicDepartment.Interfaces
+{
+    public interface ICafedraService
+    {
+        public Task<int> Create(CreateCafedraViewModel viewModel);
+
+        public Task Update(UpdateCafedraViewModel viewModel);
+
+        public Task<GetCafedraViewModel> Get(int id);
+
+        public Task<IEnumerable<GetCafedraSelectorViewModel>> GetSelector();
+
+        public Task<ApiResultViewModel<GetCafedraViewModel>> GetApiResponce(int pageIndex,
+            int pageSize,
+            IEnumerable<SortingRequest> sortingRequests = null,
+            IEnumerable<FilterRequest> filterRequests = null);
+    }
+}
---
+++ ElectronicDepartment.Interfaces/ICourseService.cs
@@ -0,0 +1,20 @@
+﻿using ElectronicDepartment.Web.Shared;
+using ElectronicDepartment.Web.Shared.Common;
+using ElectronicDepartment.Web.Shared.Course;
+using ElectronicDepartment.Web.Shared.Course.Responce;
+
+namespace ElectronicDepartment.Interfaces
+{
+    public interface ICourseService
+    {
+        public Task<int> Create(CreateCourseViewModel viewModel);
+
+        public Task Update(UpdateCourseViewModel viewModel);
+
+        public Task<GetCourseViewModel> Get(int id);
+
+        public Task<IEnumerable<GetCourseSelectorViewModel>> GetSelector();
+        
+        public Task<ApiResultViewModel<GetCourseViewModel>> GetApiResponce(int pageIndex, int pageSize, IEnumerable<SortingRequest> sortingRequests, IEnumerable<FilterRequest> filterRequests);
+    }
+}
