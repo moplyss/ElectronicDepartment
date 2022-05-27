2bc90f9d1ed5b46a10c84f8342fa3e8e700d29e1
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-27 22:51:17 +0300

    kursova

---
+++ ElectronicDepartment.BusinessLogic/Helpers/ApiResult.cs
@@ -0,0 +1,288 @@
+﻿using ElectronicDepartment.Web.Shared.Common;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ElectronicDepartment.Web.Shared
+{
+
+    public class ApiResult<T> : ApiResultViewModel<T>
+    {
+        /// <summary>
+        /// Private constructor called by the CreateAsync method.
+        /// </summary>
+        private ApiResult(
+            List<T> data,
+            int count,
+            int pageIndex,
+            int pageSize,
+            IEnumerable<SortingRequest> sortingRequests,
+            IEnumerable<FilterRequest> filterRequests)
+        {
+            Data = data;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = count;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            SortingRequests = sortingRequests;
+            FilterRequests = filterRequests;
+        }
+
+        #region Methods
+        /// <summary>
+        /// Pages, sorts and/or filters a IQueryable source.
+        /// </summary>
+        /// <param name="source">An IQueryable source of generic type</param>
+        /// <param name="pageIndex">Zero-based current page index (0 = first page)</param>
+        /// <param name="pageSize">The actual size of each page</param>
+        /// <param name="sortColumn">The sorting colum name</param>
+        /// <param name="sortOrder">The sorting order ("ASC" or "DESC")</param>
+        /// <param name="filterColumn">The filtering column name</param>
+        /// <param name="filterQuery">The filtering query (value to lookup)</param>
+        /// <returns>
+        /// A object containing the IQueryable paged/sorted/filtered result 
+        /// and all the relevant paging/sorting/filtering navigation info.
+        /// </returns>
+        public static async Task<ApiResult<T>> CreateAsync(
+            IQueryable<T> source,
+            int pageIndex,
+            int pageSize,
+            IEnumerable<SortingRequest> sortingRequests = null,
+            IEnumerable<FilterRequest> filterRequests = null)
+        {
+            if (filterRequests is not null)
+                foreach (var filterRequest in filterRequests)
+                {
+                    if (!string.IsNullOrEmpty(filterRequest.FilterColumn)
+                   && !string.IsNullOrEmpty(filterRequest.FilterQuery)
+                   && IsValidProperty(filterRequest.FilterColumn))
+                    {
+                        var type = GetPropertyValue(filterRequest.FilterColumn);
+
+                        if (type == typeof(int))
+                        {
+                            source = source.Where(
+                           string.Format("{0}.Equals(@0)",
+                           filterRequest.FilterColumn),
+                           Convert.ToInt32(filterRequest.FilterQuery));
+
+                        }
+                        else if (type == typeof(decimal))
+                        {
+                            source = source.Where(
+                          string.Format("{0}.Equals(@0)",
+                          filterRequest.FilterColumn),
+                          Convert.ToDecimal(filterRequest.FilterQuery));
+                        }
+
+                        else if (filterRequest.IsPartFilter)
+                        {
+                            source = source.Where(
+                                string.Format("{0}.Contains(@0)",
+                                filterRequest.FilterColumn),
+                                filterRequest.FilterQuery);
+                        }
+                        else
+                        {
+                            source = source.Where(
+                               string.Format("{0}.Equals(@0)",
+                               filterRequest.FilterColumn),
+                               filterRequest.FilterQuery);
+                        }
+                    }
+                }
+
+            var count = await source.CountAsync();
+
+            if (sortingRequests is not null)
+            {
+                var sortingString = String.Empty;
+
+                foreach (var sortingRequest in sortingRequests)
+                {
+                    if (!string.IsNullOrEmpty(sortingRequest.SortColumn)
+                    && IsValidProperty(sortingRequest.SortColumn))
+                    {
+                        sortingRequest.SortOrder = !string.IsNullOrEmpty(sortingRequest.SortOrder)
+                            && sortingRequest.SortOrder.ToUpper() == "ASC"
+                            ? "ASC"
+                            : "DESC";
+
+                        if (string.IsNullOrEmpty(sortingString))
+                        {
+                            sortingString += $"{sortingRequest.SortColumn} {sortingRequest.SortOrder}";
+                        }
+                        else
+                        {
+                            sortingString += $", {sortingRequest.SortColumn} {sortingRequest.SortOrder}";
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(sortingString))
+                {
+                    source = source.OrderBy(sortingString);
+                }
+            }
+            
+            source = source
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
+
+            var data = await source.ToListAsync();
+
+            return new ApiResult<T>(
+                data,
+                count,
+                pageIndex,
+                pageSize,
+                sortingRequests,
+                filterRequests);
+        }
+
+        public static async Task<ApiResult<T>> CreateAsync<T, D>(
+            Expression<Func<D, T>> selector,
+            IQueryable<D> source,
+            int pageIndex,
+            int pageSize,
+            IEnumerable<SortingRequest> sortingRequests = null,
+            IEnumerable<FilterRequest> filterRequests = null)
+        {
+            if (filterRequests is not null)
+                foreach (var filterRequest in filterRequests)
+                {
+                    if (!string.IsNullOrEmpty(filterRequest.FilterColumn)
+                   && !string.IsNullOrEmpty(filterRequest.FilterQuery)
+                   && IsValidProperty(filterRequest.FilterColumn))
+                    {
+                        var type = GetPropertyValue(filterRequest.FilterColumn);
+
+                        if (type == typeof(int))
+                        {
+                            source = source.Where(
+                           string.Format("{0}.Equals(@0)",
+                           filterRequest.FilterColumn),
+                           Convert.ToInt32(filterRequest.FilterQuery));
+
+                        }
+                        else if (type == typeof(decimal))
+                        {
+                            source = source.Where(
+                          string.Format("{0}.Equals(@0)",
+                          filterRequest.FilterColumn),
+                          Convert.ToDecimal(filterRequest.FilterQuery));
+                        }
+
+                        else if (filterRequest.IsPartFilter)
+                        {
+                            source = source.Where(
+                                string.Format("{0}.Contains(@0)",
+                                filterRequest.FilterColumn),
+                                filterRequest.FilterQuery);
+                        }
+                        else
+                        {
+                            source = source.Where(
+                               string.Format("{0}.Equals(@0)",
+                               filterRequest.FilterColumn),
+                               filterRequest.FilterQuery);
+                        }
+                    }
+                }
+
+            var count = await source.CountAsync();
+
+            if (sortingRequests is not null)
+            {
+                var sortingString = String.Empty;
+
+                foreach (var sortingRequest in sortingRequests)
+                {
+                    if (!string.IsNullOrEmpty(sortingRequest.SortColumn)
+                    && IsValidProperty(sortingRequest.SortColumn))
+                    {
+                        sortingRequest.SortOrder = !string.IsNullOrEmpty(sortingRequest.SortOrder)
+                            && sortingRequest.SortOrder.ToUpper() == "ASC"
+                            ? "ASC"
+                            : "DESC";
+
+                        if (string.IsNullOrEmpty(sortingString))
+                        {
+                            sortingString += $"{sortingRequest.SortColumn} {sortingRequest.SortOrder}";
+                        }
+                        else
+                        {
+                            sortingString += $", {sortingRequest.SortColumn} {sortingRequest.SortOrder}";
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(sortingString))
+                {
+                    source = source.OrderBy(sortingString);
+                }
+            }
+
+            source = source
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
+
+            var data = await source
+                .Select(selector)
+                .ToListAsync();
+
+            return new ApiResult<T>(
+                data,
+                count,
+                pageIndex,
+                pageSize,
+                sortingRequests,
+                filterRequests);
+        }
+
+        /// <summary>
+        /// Checks if the given property name exists
+        /// to protect against SQL injection attacks
+        /// </summary>
+        public static bool IsValidProperty(
+            string propertyName,
+            bool throwExceptionIfNotFound = true)
+        {
+            var prop = typeof(T).GetProperty(
+                propertyName,
+                BindingFlags.IgnoreCase |
+                BindingFlags.Public |
+                BindingFlags.Static |
+                BindingFlags.Instance);
+            if (prop == null && throwExceptionIfNotFound)
+                throw new NotSupportedException(
+                    string.Format(
+                        "ERROR: Property '{0}' does not exist.",
+                        propertyName)
+                    );
+            return prop != null;
+        }
+
+        public static Type GetPropertyValue(string propertyName, bool throwExceptionIfNotFound = true)
+        {
+            var prop = typeof(T).GetProperty(
+                propertyName,
+                BindingFlags.IgnoreCase |
+                BindingFlags.Public |
+                BindingFlags.Static |
+                BindingFlags.Instance);
+            if (prop == null && throwExceptionIfNotFound)
+                throw new NotSupportedException(
+                    string.Format(
+                        "ERROR: Property '{0}' does not exist.",
+                        propertyName)
+                    );
+            return prop.PropertyType;
+        }
+        #endregion
+    }
+
+}
---
+++ ElectronicDepartment.BusinessLogic/Helpers/FirstInit.cs
@@ -0,0 +1,199 @@
+﻿using ElectronicDepartment.Common.Enums;
+using ElectronicDepartment.DataAccess;
+using ElectronicDepartment.DomainEntities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using static ElectronicDepartment.Common.Constants;
+
+namespace ElectronicDepartment.BusinessLogic.Helpers
+{
+    public static class FirstInit
+    {
+        public static async Task InitRoles(RoleManager<IdentityRole> _roleManager, IEnumerable<string> roles)
+        {
+            foreach (var item in roles)
+            {
+                var res = await _roleManager.FindByNameAsync(item);
+
+                if (res is null)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(item));
+                }
+            }
+        }
+
+        public static async Task InitStartData(UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext)
+        {
+            await InitManager(userManager);
+            await InitAdmin(userManager);
+
+            await InitStudent(userManager, dbContext);
+            await InitTeacher(userManager, dbContext);
+        }
+
+        public static async Task InitStudent(UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext)
+        {
+            var password = "aStudent123!";
+            var student = new Student()
+            {
+                BirthDay = DateTime.Now,
+                Email = @"Student@gmail.com",
+                FirstName = "Student",
+                MiddleName = "Student",
+                LastName = "Student",
+                Gender = Gender.None,
+                UserName = @"Student",
+                EmailConfirmed = true,
+                UserType = UserType.Student,
+                PhoneNumber = "++3777777",
+                PhoneNumberConfirmed = true
+            };
+
+            var user = await userManager.FindByEmailAsync(student.Email);
+
+            if (user is null)
+            {
+                await InitGroup(dbContext);
+                student.Group = await dbContext.Groups.FirstOrDefaultAsync();
+
+                var result = await userManager.CreateAsync(student, password);
+            }
+
+            var roles = await userManager.GetRolesAsync(student);
+
+            if (roles.Count() == 0)
+            {
+                await userManager.AddToRoleAsync(student, STUDENTROLE);
+            }
+        }
+
+        public static async Task InitManager(UserManager<ApplicationUser> userManager)
+        {
+            var password = "aManager123!";
+            var manager = new Manager()
+            {
+                BirthDay = DateTime.Now,
+                Email = @"Manager@gmail.com",
+                FirstName = "Manager",
+                MiddleName = "Manager",
+                LastName = "Manager",
+                Gender = Gender.None,
+                UserName = @"Manager",
+                EmailConfirmed = true,
+                UserType = UserType.Manager,
+                PhoneNumber = "++35555555",
+                PhoneNumberConfirmed = true
+            };
+
+            var user = await userManager.FindByEmailAsync(manager.Email);
+
+            if (user is null)
+            {
+                var result = await userManager.CreateAsync(manager, password);
+            }
+
+            var roles = await userManager.GetRolesAsync(manager);
+
+            if (roles.Count() == 0)
+            {
+                await userManager.AddToRoleAsync(manager, MANAGERROLE);
+            }
+        }
+
+        public static async Task InitTeacher(UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext)
+        {
+            var password = "aTeacher123!";
+            var teacher = new Teacher()
+            {
+                BirthDay = DateTime.Now,
+                Email = @"Teacher@gmail.com",
+                FirstName = "Teacher",
+                MiddleName = "Teacher",
+                LastName = "Teacher",
+                Gender = Gender.None,
+                UserName = @"Teacher",
+                EmailConfirmed = true,
+                UserType = UserType.Teacher,
+                PhoneNumber = "++389999999",
+                PhoneNumberConfirmed = true
+            };
+
+            var user = await userManager.FindByEmailAsync(teacher.Email);
+
+            if (user is null)
+            {
+                await InitCafedra(dbContext);
+                var cafedra = await dbContext.Cafedras.FirstOrDefaultAsync();
+
+                var result = await userManager.CreateAsync(teacher, password);
+            }
+
+            var roles = await userManager.GetRolesAsync(teacher);
+
+            if (roles.Count() == 0)
+            {
+                await userManager.AddToRoleAsync(teacher, TEACHERROLE);
+            }
+        }
+
+        public static async Task InitAdmin(UserManager<ApplicationUser> userManager)
+        {
+            var password = "admin123!";
+            var admin = new Admin()
+            {
+                BirthDay = DateTime.Now,
+                Email = @"admin123@gmail.com",
+                FirstName = "Admin",
+                MiddleName = "Admin",
+                LastName = "Admin",
+                Gender = Gender.None,
+                UserName = @"adminAdmin",
+                EmailConfirmed = true,
+                UserType = UserType.Admin,
+                PhoneNumber = "+380111111111",
+                PhoneNumberConfirmed = true
+            };
+
+            var user = await userManager.FindByEmailAsync(admin.Email);
+
+            if (user is null)
+            {
+                var result = await userManager.CreateAsync(admin, password);
+            }
+
+            var roles = await userManager.GetRolesAsync(admin);
+
+            if (roles.Count() == 0)
+            {
+                await userManager.AddToRoleAsync(admin, ADMINROLE);
+            }
+        }
+
+        private static async Task InitGroup(ApplicationDbContext dbContext)
+        {
+            var group1 = new Group()
+            {
+                Name = "PP-22-ZZ",
+            };
+
+
+            await dbContext.Groups.AddAsync(group1);
+
+            await dbContext.SaveChangesAsync();
+        }
+
+        public static async Task InitCafedra(ApplicationDbContext dbContext)
+        {
+            var cafedra1 = new Cafedra()
+            {
+                Name = "Botanica",
+                Description = "Cafedra Botanici",
+                Phone = "+3809944622",
+            };
+
+            await dbContext.Cafedras.AddAsync(cafedra1);
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
