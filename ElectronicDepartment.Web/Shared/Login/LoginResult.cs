941144b55b96ab6beced1328a91972bd043e77cf
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:47:20 +0300

    kursova

--- ElectronicDepartment.Web/Shared/Login/LoginResult.cs
+++ ElectronicDepartment.Web/Shared/Login/LoginResult.cs
@@ -1,1 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ElectronicDepartment.Web.Shared.Login
+{
+	public class LoginResult
+	{
+		public string Message { get; set; }
+		
+		public string Email { get; set; }
+		
+		public string JWTBearer { get; set; }
+		
+		public bool Success { get; set; }
+		
+		public string Role { get; set; }
+	}
+
+	public class LoginModel
+	{
+		[Required(ErrorMessage = "Email is required.")]
+		[EmailAddress(ErrorMessage = "Email address is not valid.")]
+		public string Email { get; set; } // NOTE: email will be the username, too
+
+		[Required(ErrorMessage = "Password is required.")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
