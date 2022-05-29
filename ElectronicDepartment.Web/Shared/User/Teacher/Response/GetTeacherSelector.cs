3ab870bce4d7d44d62eb7e1903b8234b90f4cf22
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:27:54 +0300

    kursova

---
+++ ElectronicDepartment.Web/Shared/User/Teacher/Response/GetTeacherSelector.cs
@@ -0,0 +1,24 @@
+﻿using ElectronicDepartment.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDepartment.Web.Shared.User.Teacher.Responce
+{
+    public class GetTeacherSelector
+    {
+        public int Id { get; set; }
+
+        public int CafedraId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public AcademicAcredition AcademicAcredition { get; set; }
+    }
+}
---
+++ ElectronicDepartment.Web/Shared/User/Teacher/Response/GetTeacherViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ElectronicDepartment.Common.Enums;
+
+namespace ElectronicDepartment.Web.Shared.User.Teacher.Responce
+{
+    public class GetTeacherViewModel : GetBaseUserViewModel
+    {
+        public AcademicAcredition AcademicAcredition { get; set; }
+    }
+}
