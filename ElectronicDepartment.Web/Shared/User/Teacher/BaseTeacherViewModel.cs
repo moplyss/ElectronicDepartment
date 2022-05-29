7568704d5a781c7123bc7202df4df662c19ca35a
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:26:47 +0300

    kursova

--- ElectronicDepartment.Web/Shared/User/Teacher/BaseTeacherViewModel.cs
+++ ElectronicDepartment.Web/Shared/User/Teacher/BaseTeacherViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ElectronicDepartment.Common.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace ElectronicDepartment.Web.Shared.User.Teacher
 {
     public class BaseTeacherViewModel : BaseUserViewModel
     {
-        public AcademicAcredition AcademicAcredition { get; set; }
+        [Required]
+        public int? CafedraId { get; set; }
+
+        public AcademicAcredition AcademicAcredition { get; set; } = AcademicAcredition.None;
     }
 }
