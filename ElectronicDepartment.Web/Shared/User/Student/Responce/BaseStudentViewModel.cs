596b4a8d4562ec549e367719192f429deb2e204d
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:25:48 +0300

    kursova

---
+++ ElectronicDepartment.Web/Shared/User/Student/Responce/BaseStudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDepartment.Web.Shared.User.Student.Responce
+{
+
+    public class BaseStudentViewModel : BaseUserViewModel
+    {
+        public int GroupId { get; set; }
+    }
+}
---
+++ ElectronicDepartment.Web/Shared/User/Student/Responce/GetStudentViewModel.cs
@@ -0,0 +1,39 @@
+﻿using ElectronicDepartment.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDepartment.Web.Shared.User.Student.Responce
+{
+    public class GetStudentViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string FirstName { get; set; } = default!;
+
+        [Required]
+        [MaxLength(128)]
+        public string LastName { get; set; } = default!;
+
+        [Required]
+        [MaxLength(128)]
+        public string MiddleName { get; set; } = default!;
+
+        public DateTime BirthDay { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public string Email { get; set; } = default!;
+
+        public string PhoneNumber { get; set; }
+
+        public int GroupId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
