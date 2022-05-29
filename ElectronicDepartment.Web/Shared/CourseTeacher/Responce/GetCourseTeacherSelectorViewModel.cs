521b440e8cd8662d6b8f774b0bc3e2f56e13ec35
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:43:33 +0300

    kursova

---
+++ ElectronicDepartment.Web/Shared/CourseTeacher/Responce/GetCourseTeacherSelectorViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ElectronicDepartment.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDepartment.Web.Shared.CourseTeacher.Responce
+{
+    public class GetCourseTeacherSelectorViewModel
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public AcademicAcredition AcademicAcredition { get; set; }
+    }
+}
--- ElectronicDepartment.Web/Shared/CourseTeacher/Responce/GetCourseTeacherViewModel.cs
+++ ElectronicDepartment.Web/Shared/CourseTeacher/Responce/GetCourseTeacherViewModel.cs
@@ -1,1 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ElectronicDepartment.Web.Shared.CourseTeacher.Responce
+{
+    public class GetCourseTeacherViewModel
+    {
+        public int Id { get; set; }
+
+        public int CourseId { get; set; }
+
+        public string TeacherId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
