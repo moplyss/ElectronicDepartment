dda3c131b319b8a6e8e5255c45792a411d6216cb
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:21:14 +0300

    kursova

---
+++ ElectronicDepartment.Web/Shared/Course/Response/GetCourseSelectorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectronicDepartment.Web.Shared.Course.Responce
+{
+    public class GetCourseSelectorViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
