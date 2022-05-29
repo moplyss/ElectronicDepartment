db9f030c51270c92f701063223a911e371970663
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:39:32 +0300

    kursova

--- ElectronicDepartment.Web/Shared/Cafedra/Responce/GetCafedraSelectorViewModel.cs
+++ ElectronicDepartment.Web/Shared/Cafedra/Responce/GetCafedraSelectorViewModel.cs
@@ -1,1 +1,9 @@
+﻿namespace ElectronicDepartment.Web.Shared.Cafedra.Responce
+{
+    public class GetCafedraSelectorViewModel
+    {
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+    }
+}
