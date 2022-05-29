7e6d82b0630b2724ec798cd7166699739899e51d
Anastasiiy <103684530+Anastasiiy@users.noreply.github.com>
2022-05-29 21:15:52 +0300

    kursova

---
+++ ElectronicDepartment.Web/Server/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json.Serialization;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using ElectronicDepartment.Common.Exceptions;
+
+namespace Company.WebApplication1
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, IWebHostEnvironment env, ILogger logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "text/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        var exception = contextFeature.Error;
+                        var path = context.Request.Path;
+                        var message = string.Empty;
+                        var result = string.Empty;
+                        dynamic response = null;
+
+                        switch (exception)
+                        {
+                            case DbNullReferenceException controllerException:
+                                logger.LogError("Invalid data send no elements with such indexes");
+                                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                                message = exception.Message;
+                                break;
+                                break;
+                            default:
+                                logger.LogError(exception, context.Request.Path);
+                                message = exception.Message;
+                                break;
+                        }
+
+                        if (env.EnvironmentName == "Test" || env.IsDevelopment())
+                        {
+                            response = new
+                            {
+                                StatusCode = context.Response.StatusCode,
+                                ExMessage = message,
+                                response,
+                                exception
+                            };
+                        }
+                        else
+                        {
+                            response = new
+                            {
+                                StatusCode = context.Response.StatusCode,
+                                ExMessage = message,
+                                response
+                            };
+                        }
+                    }
+                });
+            });
+        }
+    }
+}
--- ElectronicDepartment.Web/Server/Program.cs
+++ ElectronicDepartment.Web/Server/Program.cs
@@ -1,4 +1,18 @@
+using ElectronicDepartment.BusinessLogic;
+using ElectronicDepartment.DataAccess;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Proxies;
+using Microsoft.Extensions.DependencyInjection;
+using ElectronicDepartment.DomainEntities;
+using Microsoft.AspNetCore.Identity;
+using static ElectronicDepartment.BusinessLogic.Helpers.FirstInit;
+using static ElectronicDepartment.Common.Constants;
+using ElectronicDepartment.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using static Company.WebApplication1.StartupConfiguration;
 
 namespace Company.WebApplication1
 {
@@ -7,17 +21,63 @@ namespace Company.WebApplication1
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseLazyLoadingProxies()
+                .UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString")));
 
+            builder.Services.AddIdentityCore<ApplicationUser>(opts =>
+            {
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireLowercase = false;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // указывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        // строка, представляющая издателя
+                        ValidIssuer = AuthOptions.ISSUER,
+                        // будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        // установка потребителя токена
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        // будет ли валидироваться время существования
+                        ValidateLifetime = true,
+                        // установка ключа безопасности
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        // валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
+
+            builder.Services.AddInjection();
             // Add services to the container.
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
+
+            builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.RoutePrefix = ("swagger/docs");
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
                 app.UseWebAssemblyDebugging();
             }
             else
@@ -27,6 +87,8 @@ namespace Company.WebApplication1
                 app.UseHsts();
             }
 
+            app.ConfigureExceptionHandler(app.Environment, app.Logger);
+
             app.UseHttpsRedirection();
 
             app.UseBlazorFrameworkFiles();
@@ -34,12 +96,57 @@ namespace Company.WebApplication1
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapRazorPages();
+            app.MapDefaultControllerRoute();
             app.MapControllers();
             app.MapFallbackToFile("index.html");
 
+            InitDb(app);
+
+            
+
             app.Run();
         }
+    }
+
+    public static class StartupConfiguration
+    {
+        public static void AddInjection(this IServiceCollection services)
+        {
+            services.AddScoped<IUserManagerService, ManagerService>();
+            services.AddScoped<ICafedraService, CafedraService>();
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ICourseTeacherService, CourseTeacherService>();
+            services.AddScoped<ILessonService, LessonService>();
+            services.AddScoped<IStudentOnLessonService, StudentOnLessonService>();
+            services.AddScoped<IGroupService, GroupService>();
+        }
+
+        public static void InitDb(WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                //Resolve ASP .NET Core Identity with DI help
+                var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                context.Database.EnsureCreated();
+
+                InitRoles(roleManager, GetRoles()).GetAwaiter().GetResult();
+                InitStartData(userManager, context).GetAwaiter().GetResult();
+            }
+            using (var scope = app.Services.CreateScope())
+            {
+                //Resolve ASP .NET Core Identity with DI help
+                var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+            }
+        }
+
     }
 }
